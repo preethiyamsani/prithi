4344e09e4a93d25e4ba2ef592840096e2e535898
Preethi Yamsani <pree.yamsani@gmail.com>
2019-03-11 10:32:39 +0530

    test commit

--- glt_test/src/test/helloworld1.java
+++ glt_test/src/test/helloworld1.java
@@ -1,10 +1,10 @@
 package test;
 
-public class helloworld {
+public class helloworld1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello World");
+
 	}
 
 }
